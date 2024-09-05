86b2cb6720ec9ac6bce043b9d13931cac81268b8
Satheez <satheez@efuturesworld.com>
2024-09-05 22:39:21 +0530

    Adding livewire support Not completed

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -19,6 +19,7 @@
     </div>
 
     @livewireScripts
+    <script src="//unpkg.com/alpinejs" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
